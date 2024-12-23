b69424e586876558a772d19774655ec66a1dc8f3
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2024-12-23 10:04:44 +0100

    1.1.0 update

--- ascii/src/main/java/com/ascii/AsciiMain.java
+++ ascii/src/main/java/com/ascii/AsciiMain.java
@@ -5,7 +5,7 @@ package com.ascii;
  * It initializes the GUI for the application.
  * 
  * @author Artem Sinchuk
- * @version 1.0
+ * @version 1.1
  */
 public class AsciiMain {
   public static void main(String[] args) {
