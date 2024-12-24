121135af594d90fb25356ba91665a173a720a487
Artem Sinchuk <158162880+ArtemSinchuk@users.noreply.github.com>
2024-12-24 14:54:06 +0100

    refactoring 2.0

--- ascii/src/main/java/com/ascii/AsciiGUI.java
+++ ascii/src/main/java/com/ascii/AsciiGUI.java
@@ -7,15 +7,10 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.event.*;
 
 public class AsciiGUI extends JFrame {
-    private JTextArea textField0 = new JTextArea(5, 30);
-    private JTextArea textField1 = new JTextArea(5, 30);
-    private JTextArea asciiField = new JTextArea(5, 30);
-    private JButton copyButton = new JButton("Select and Copy");
-    private JButton copyButton2 = new JButton("Select and Copy");
-    private JButton changeTheme = new JButton();
-    private JLabel englishText = new JLabel("Enter Text");
-    private JLabel rusText = new JLabel("Translate");
-    private JLabel asciiCode = new JLabel("ASCII Code");
+    private JTextArea InputTextArea = new JTextArea(5, 30), translatedTextArea = new JTextArea(5, 30), asciiTextArea = new JTextArea(5, 30);
+    private JButton copyTranslationButton = new JButton("Select and Copy"), copyAsciiButton = new JButton("Select and Copy"), changeThemeButton = new JButton();
+    private JLabel englishTextLabel = new JLabel("Enter Text"), rusTextLabel = new JLabel("Translate"), asciiCodeLabel = new JLabel("ASCII Code"),imageTest = new JLabel();
+    ImageIcon themeMoon = new ImageIcon("moon.png");
 
     public AsciiGUI() {
         config();
@@ -31,63 +26,59 @@ public class AsciiGUI extends JFrame {
         getContentPane().setBackground(new Color(0xd3d3d3));
         setLocationRelativeTo(null);
         setLayout(null);
-        setVisible(true);
         addComponents();
+        setVisible(true);
     }
 
     private void addComponents() {
-        add(englishText);
-        add(rusText);
-        add(asciiCode);
-        add(textField0);
-        add(textField1);
-        add(asciiField);
-        add(copyButton);
-        add(copyButton2);
-        add(changeTheme);
+        add(englishTextLabel);
+        add(rusTextLabel);
+        add(asciiCodeLabel);
+        add(InputTextArea);
+        add(translatedTextArea);
+        add(asciiTextArea);
+        add(copyTranslationButton);
+        add(copyAsciiButton);
+        add(changeThemeButton);
         add(imageTest);
     }
 
     public void configureButtons() {
-        copyButton.setBounds(200, 225, 150, 30);
-        copyButton.setText("Select and Copy");
-        copyButton.setBorderPainted(false);
-        copyButton.setBackground(Color.WHITE);
-        
-        copyButton2.setBounds(400, 225, 150, 30);
-        copyButton2.setText("Select and Copy");
-        copyButton2.setBorderPainted(false);
-        copyButton2.setBackground(Color.WHITE);
-        
-        changeTheme.setBounds(10, 10, 150, 30);
+        configureButtons(copyTranslationButton, 200, 225, 150, 30);
+        configureButtons(copyAsciiButton, 400, 225, 150, 30);
+        configureButtons(changeThemeButton, 10, 10, 150, 30);
         // changeTheme.setOpaque(false);
         // changeTheme.setContentAreaFilled(false);
-        // changeTheme.setBorderPainted(false);
+        changeThemeButton.setIcon(themeMoon);
     }
 
     public void configureLabels() {
-        englishText.setBounds(10, 50, 150, 30);
-        rusText.setBounds(200, 50, 150, 30);
-        asciiCode.setBounds(400, 50, 150, 30);
+        englishTextLabel.setBounds(10, 50, 150, 30);
+        rusTextLabel.setBounds(200, 50, 150, 30);
+        asciiCodeLabel.setBounds(400, 50, 150, 30);
         imageTest.setBounds(200, 10, 150, 30);
+        imageTest.setIcon(themeMoon);
     }
 
     public void configureTextArea() {
-        textField0.setBounds(10, 100, 150, 100);
-        textField0.setFont(new Font("Arial", Font.PLAIN, 12));
-        textField0.setLineWrap(true);
-        
-        textField1.setBounds(200, 100, 150, 100);
-        textField1.setFont(new Font("Arial", Font.PLAIN, 12));
-        textField1.setLineWrap(true);
-        
-        asciiField.setBounds(400, 100, 150, 100);
-        asciiField.setFont(new Font("Arial", Font.PLAIN, 12));
-        asciiField.setLineWrap(true);
-        asciiField.setEditable(false);
+        configureTextArea(InputTextArea, 10, 100, 150, 100);
+        configureTextArea(translatedTextArea, 200, 100, 150, 100);
+        configureTextArea(asciiTextArea, 400, 100, 150, 100);
+        asciiTextArea.setEditable(false);
     }
 
-     public void addCopyButtonListeners() {
+    public void configureButtons(JButton btn, int x, int y, int width, int height) {
+        btn.setBounds(x, y, width, height);
+        btn.setBorderPainted(false);
+        btn.setBackground(Color.WHITE);
+    }
+    public void configureTextArea(JTextArea area, int x, int y, int width, int height) {
+        area.setBounds(x, y, width, height);
+        area.setFont(new Font("Arial", Font.PLAIN, 12));
+        area.setLineWrap(true);
+    }
+
+    public void addCopyButtonListeners() {
         copyTranslationButton.addActionListener(e -> {
             String text = translatedTextArea.getText();
             try {
@@ -106,7 +97,7 @@ public class AsciiGUI extends JFrame {
             }
         });
 
-        changeTheme.addActionListener(e -> {
+        changeThemeButton.addActionListener(e -> {
             changeTheme();
         });
     }
@@ -120,7 +111,7 @@ public class AsciiGUI extends JFrame {
 
     private boolean updating = false;
     private void addDocListeners() {
-        textField0.getDocument().addDocumentListener(new DocumentListener() {
+        InputTextArea.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
                 updateFieldsFromLeft();
@@ -141,15 +132,15 @@ public class AsciiGUI extends JFrame {
                     return; // If the update is already in progress, exit
                 updating = true;
 
-                String input = textField0.getText();
-                textField1.setText(Translate.translateText(input));
-                asciiField.setText(AsciiConverter.toAscii(input));
+                String input = InputTextArea.getText();
+                translatedTextArea.setText(Translate.translateText(input));
+                asciiTextArea.setText(AsciiConverter.toAscii(input));
 
                 updating = false;
             }
         });
 
-        textField1.getDocument().addDocumentListener(new DocumentListener() {
+        translatedTextArea.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
                 updateFieldsFromMiddle();
@@ -170,8 +161,8 @@ public class AsciiGUI extends JFrame {
                     return;
                 updating = true;
 
-                String input = textField1.getText();
-                asciiField.setText(AsciiConverter.toAscii(input));
+                String input = translatedTextArea.getText();
+                asciiTextArea.setText(AsciiConverter.toAscii(input));
 
                 updating = false;
             }
@@ -190,51 +181,37 @@ public class AsciiGUI extends JFrame {
         this.setFocusable(true);
     }
 
-    int changes = 1;
+    private boolean isDarkTheme;
     public void changeTheme() {
-        if (changes % 2 == 1) {
+        if (!isDarkTheme) {
             setDarkTheme();
         } else {
             setLightTheme();
         }
-        changes++;
+        isDarkTheme = !isDarkTheme;
     }
 
     public void setDarkTheme() {
-        getContentPane().setBackground(new Color(0x181818));
-        copyButton.setBackground(new Color(0x313232));
-        copyButton.setForeground(new Color(0xc3c3c3));
-
-        copyButton2.setBackground(new Color(0x313232));
-        copyButton2.setForeground(new Color(0xc3c3c3));
-
-        textField0.setBackground(new Color(0x1f1f1f));
-        textField1.setBackground(new Color(0x1f1f1f));
-        asciiField.setBackground(new Color(0x1f1f1f));
-        textField0.setForeground(new Color(0xc3c3c3));
-        textField1.setForeground(new Color(0xc3c3c3));
-        asciiField.setForeground(new Color(0xc3c3c3));
-
-        englishText.setForeground(new Color(0xc3c3c3));
-        rusText.setForeground(new Color(0xc3c3c3));
-        asciiCode.setForeground(new Color(0xc3c3c3));
+        configureTheme(new Color(0x181818), new Color(0xc3c3c3), new Color(0x1f1f1f));
     }
 
     public void setLightTheme() {
-        getContentPane().setBackground(new Color(0xd3d3d3));
-
-        copyButton.setBackground(Color.WHITE);
-        copyButton.setForeground(Color.BLACK);
-
-        copyButton2.setBackground(Color.WHITE);
-        copyButton2.setForeground(Color.BLACK);
-
-        textField0.setBackground(Color.WHITE);
-        textField1.setBackground(Color.WHITE);
-        asciiField.setBackground(Color.WHITE);
-
-        englishText.setForeground(Color.BLACK);
-        rusText.setForeground(Color.BLACK);
-        asciiCode.setForeground(Color.BLACK);
+        configureTheme(new Color(0xd3d3d3), Color.BLACK, Color.WHITE);
+    }
+    public void configureTheme(Color bg, Color fg, Color textAreaBg) {
+        getContentPane().setBackground(bg);
+        copyAsciiButton.setBackground(textAreaBg);
+        copyAsciiButton.setForeground(fg);
+        copyTranslationButton.setBackground(textAreaBg);
+        copyTranslationButton.setForeground(fg);
+        InputTextArea.setBackground(textAreaBg);
+        InputTextArea.setForeground(fg);
+        translatedTextArea.setBackground(textAreaBg);
+        translatedTextArea.setForeground(fg);
+        asciiTextArea.setBackground(textAreaBg);
+        asciiTextArea.setForeground(fg);
+        englishTextLabel.setForeground(fg);
+        rusTextLabel.setForeground(fg);
+        asciiCodeLabel.setForeground(fg);
     }
 }
